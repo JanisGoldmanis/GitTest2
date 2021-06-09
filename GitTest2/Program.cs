ff3bae113ef72070048094d7f50064f977baccf8
Jānis Goldmanis <janis.goldmanis@gmail.com>
2021-06-09 13:54:36 +0300

    Hello2

--- GitTest2/Program.cs
+++ GitTest2/Program.cs
@@ -7,6 +7,7 @@ namespace GitTest2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello to you too!!");
             Console.ReadLine();
         }
     }
